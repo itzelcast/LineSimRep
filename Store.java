7643199b39a255014688b44855ae03b616cd0bf3
josemrtnz <49460561+josemrtnz@users.noreply.github.com>
2019-04-18 17:19:44 -0700

    Lmao

--- Store.java
+++ Store.java
@@ -4,7 +4,9 @@ public class Store{
 
 	int numberOfQueues;
 	int totalDElay;
-
+	public void printSomething(){
+		System.out.println("Hello monkey!");
+	}
 	// will probably need an add method that adds a Person to the PersonQueue with the
     //least number of people
     //will probably need a remove method that removes a person from a PersonQueue who
