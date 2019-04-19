617a9af8f07ddfaa949701fb42c05d37d94e3ff2
josemrtnz <49460561+josemrtnz@users.noreply.github.com>
2019-04-18 18:25:47 -0700

    PersonContent
    Creating methods for person class

--- Person.java
+++ Person.java
@@ -1,5 +1,15 @@
 public class Person{//corresponds to a customer
-	int arrivalTime;
-	int departureTime;
-	int processingTime;
+	private int arrivalTime;
+	private int departureTime;
+	private int processingTime;
+
+	public Person(int a, int d, int p){
+		arrivalTime = a;
+		departureTime = d;
+		processingTime = p;
+	}
+
+	public int getATime(){return arrivalTime;}
+	public int getDTime(){return departureTime;}
+	public int getPTime(){return processingTime;}
 }
