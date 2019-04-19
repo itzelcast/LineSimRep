9763eddcdbed95b9384a3af4d26bc5be8e9f770a
josemrtnz <49460561+josemrtnz@users.noreply.github.com>
2019-04-18 18:30:08 -0700

    TestPerson
    A test to see that our person class is working. It works!

--- Driver.java
+++ Driver.java
@@ -1,5 +1,8 @@
 public class Driver{
 	public static void main(String [] args){
-		System.out.print("Hello");
+		Person sk = new Person(4, 5, 6);
+		System.out.println(sk.getATime());
+		System.out.println(sk.getDTime());
+		System.out.println(sk.getPTime());
 	}
 }
