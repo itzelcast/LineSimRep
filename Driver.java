3fd491aac7c6faa65dff851eb995ba3b2ee4a075
josemrtnz <49460561+josemrtnz@users.noreply.github.com>
2019-04-27 12:22:40 -0700

    Main Mod
    Started wokring on main method and finished inputs for it.

--- Driver.java
+++ Driver.java
@@ -2,6 +2,21 @@ import java.util.*;
 
 public class Driver{
 	public static void main(String [] args){
-		Store shop = new Store(2);
+
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println("\nWelcome to Line Simulator. \nHow many queues do you want to simulate?");
+		int nLine = keyboard.nextInt();
+		System.out.println("How many customers do you want to simulate?");
+		int customers = keyboard.nextInt();
+		System.out.println("What is minimum time between person arrival?");
+		int minArrival = keyboard.nextInt();
+		System.out.println("What is the maximum time between person arrival?");
+		int maxArrival = keyboard.nextInt();
+		System.out.println("What is the minimum processing time for a person?");
+		int minProcessing = keyboard.nextInt();
+		System.out.println("What is the maximum processing time for a person?");
+		int maxProcessing = keyboard.nextInt();
+		
 	}
 }
--- Store.java
+++ Store.java
@@ -5,7 +5,7 @@ public class Store{
 	int numberOfQueues;
 	int totalDelay;
 	LinkedList<Person>[] lines;
-	
+
 	public Store(int q){
 		numberOfQueues = q;
 		lines = new LinkedList[q];
@@ -13,6 +13,14 @@ public class Store{
 		for(int i=0; i<q; i++){
 			lines[i] = new LinkedList<Person>();
 		}
+	}
+
+	public void addPerson(){
+		System.out.println(lines[0].size());
+	}
+
+	public void removePerson(){
+
 	}
 	// will probably need an add method that adds a Person to the PersonQueue with the
     //least number of people
